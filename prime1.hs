672b1bb42d04c8ecb9db41ff398dae04874a411f
Rodrigo Setti <rsetti@yahoo-inc.com>
2011-04-29 23:32:36 -0300

    primes optimization, yet, time exceeded result :(

--- prime1.hs
+++ prime1.hs
@@ -6,13 +6,14 @@ main = do tests <- getLine
           mapM_ (printPrimesBetween . (map read) . words) $ take (read tests) (lines contents)
 
 printPrimesBetween :: Integral a => [a] -> IO ()
-printPrimesBetween [a,b] = do mapM_ (putStrLn . show) $ takeWhile (<=b) $ filter (>=a) primes
+printPrimesBetween [a,b] = do mapM_ (putStrLn . show) $ filter (>=a) $ primesTo b
                               putChar '\n'
 
--- | A infinite list of primes
-primes :: Integral a => [a]
-primes = sieve [2..]
+primesTo :: Integral a => a -> [a]
+primesTo n | n > 1 = 2:(sieve [3,5..n])
+           | otherwise = []
 
 -- | The Sieve of Eratosthenes for prime calculation
 sieve :: Integral a => [a] -> [a]
+sieve [] = []
 sieve (p:ps) | p > 1 = p:(sieve $ filter ((> 0) . (`mod` p)) ps)
